ca03991685d97f4fc765bdc7b5ad9781d675530c
Pic Aurélia <pic.aurelia@gmail.com>
2025-05-27 15:37:38 +0200

    TP 04 Done

--- src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -1,9 +1,8 @@
 package fr.diginamic.hello.controleurs;
 
 import fr.diginamic.hello.entities.Ville;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +11,9 @@ import java.util.List;
 @RequestMapping("/villes")
 public class VilleControleur {
 
-    @GetMapping
-    public List<Ville> getVilles() {
-        List<Ville> villes = new ArrayList<>();
+    private List<Ville> villes = new ArrayList<>();
 
+    public VilleControleur() {
         villes.add(new Ville("Paris", 2161000));
         villes.add(new Ville("Marseille", 861635));
         villes.add(new Ville("Lyon", 513275));
@@ -26,7 +24,23 @@ public class VilleControleur {
         villes.add(new Ville("Strasbourg", 277270));
         villes.add(new Ville("Bordeaux", 252040));
         villes.add(new Ville("Lille", 232741));
+    }
 
+    @GetMapping
+    public List<Ville> getVilles() {
         return villes;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> ajouterVille(@RequestBody Ville nouvelleVille){
+
+        boolean villeExiste = villes.stream().anyMatch(v -> v.getNom().equalsIgnoreCase(nouvelleVille.getNom()));
+
+        if(villeExiste){
+            return ResponseEntity.badRequest().body("la ville existe déjà");
+        }
+        villes.add(nouvelleVille);
+
+        return ResponseEntity.ok("ville insérée avec succés.");
     }
 }
