fd3fb3839acd21e8192aff2ee43ad46c463525d1
Pic Aurélia <pic.aurelia@gmail.com>
2025-05-27 16:09:56 +0200

    TP 05 Done

--- src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -14,16 +14,16 @@ public class VilleControleur {
     private List<Ville> villes = new ArrayList<>();
 
     public VilleControleur() {
-        villes.add(new Ville("Paris", 2161000));
-        villes.add(new Ville("Marseille", 861635));
-        villes.add(new Ville("Lyon", 513275));
-        villes.add(new Ville("Toulouse", 471941));
-        villes.add(new Ville("Nice", 342522));
-        villes.add(new Ville("Nantes", 309346));
-        villes.add(new Ville("Montpellier", 285121));
-        villes.add(new Ville("Strasbourg", 277270));
-        villes.add(new Ville("Bordeaux", 252040));
-        villes.add(new Ville("Lille", 232741));
+        villes.add(new Ville(1, "Paris", 2161000));
+        villes.add(new Ville(2, "Marseille", 861635));
+        villes.add(new Ville(3, "Lyon", 513275));
+        villes.add(new Ville(4, "Toulouse", 471941));
+        villes.add(new Ville(5, "Nice", 342522));
+        villes.add(new Ville(6, "Nantes", 309346));
+        villes.add(new Ville(7, "Montpellier", 285121));
+        villes.add(new Ville(8, "Strasbourg", 277270));
+        villes.add(new Ville(9, "Bordeaux", 252040));
+        villes.add(new Ville(10, "Lille", 232741));
     }
 
     @GetMapping
@@ -34,13 +34,66 @@ public class VilleControleur {
     @PostMapping
     public ResponseEntity<String> ajouterVille(@RequestBody Ville nouvelleVille){
 
-        boolean villeExiste = villes.stream().anyMatch(v -> v.getNom().equalsIgnoreCase(nouvelleVille.getNom()));
+        boolean idExiste = villes.stream().anyMatch(v -> v.getId() == nouvelleVille.getId());
+        if (idExiste) {
+            return ResponseEntity.badRequest().body("Une ville avec cet ID existe déjà");
+        }
 
+        boolean villeExiste = villes.stream().anyMatch(v -> v.getNom().equalsIgnoreCase(nouvelleVille.getNom()));
         if(villeExiste){
-            return ResponseEntity.badRequest().body("la ville existe déjà");
+            return ResponseEntity.badRequest().body("La ville existe déjà");
         }
         villes.add(nouvelleVille);
 
-        return ResponseEntity.ok("ville insérée avec succés.");
+        return ResponseEntity.ok("ville insérée avec succès.");
     }
+
+
+    // Get par l'id
+    @GetMapping("/{id}")
+    public ResponseEntity<Ville> getVilleParId(@PathVariable int id) {
+        boolean villeExiste = villes.stream().anyMatch(v -> v.getId() == id);
+
+        if (villeExiste) {
+            Ville ville = villes.stream()
+                    .filter(v -> v.getId() == id)
+                    .findFirst()
+                    .get();
+            return ResponseEntity.ok(ville);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Put par Id
+    @PutMapping("/{id}")
+    public ResponseEntity<String> modifierVille(@PathVariable int id, @RequestBody Ville villeModifiee){
+
+        boolean idExiste = villes.stream().anyMatch(v -> v.getId() == id);
+        if (idExiste) {
+            Ville ville = villes.stream()
+                    .filter(v -> v.getId() == id)
+                    .findFirst()
+                    .get();
+
+            ville.setNom(villeModifiee.getNom());
+            ville.setNbHabitants(villeModifiee.getNbHabitants());
+
+            return ResponseEntity.ok("Ville modifiée avec succès");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // Delete avec Id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> supprimerVille(@PathVariable int id) {
+        boolean villeExiste = villes.removeIf(v -> v.getId() == id);
+
+        if (villeExiste) {
+            return ResponseEntity.ok("Ville supprimée avec succès");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
 }
--- src/main/java/fr/diginamic/hello/entities/Ville.java
+++ src/main/java/fr/diginamic/hello/entities/Ville.java
@@ -1,16 +1,30 @@
 package fr.diginamic.hello.entities;
 
 public class Ville {
-
+    private int id;
     private String nom;
     private int nbHabitants;
 
     public Ville() {
     }
 
+    public Ville(int id, String nom, int nbHabitants) {
+        this.id = id;
+        this.nom = nom;
+        this.nbHabitants = nbHabitants;
+    }
+
     public Ville(String nom, int nbHabitants) {
         this.nom = nom;
         this.nbHabitants = nbHabitants;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNom() {
